a90652e7d9ac58c75a8b5246b87f01811cd15d3b
Carme Tarter <carme.tarter@canmas.com>
2020-06-19 05:43:57 +0200

    iniciando clase Nave y mobimiento

--- main.py
+++ main.py
@@ -1,29 +1,62 @@
 import pygame as pg
 from pygame.locals import *
-import sys
+import sys, random
 
 BACKGRAUND = (0,0,0)
 YELLOW = (255, 255, 0)
+WHITE= (255, 255, 255)
 
 class Nave:
-    pass
+    def __init__(self):
+        self.vy = 1
+        self.vx = 0
+        self.w =60
+        self.h =20 
+        self.Cx = 35
+        self.Cy = 300
+
+        self.image = pg.Surface((self.w, self.h))
+        self.image.fill(WHITE)
+
+    @property
+    def posx(self):
+        return self.Cx - self.w //2
+    
+    @property
+    def posy(self):
+        return self.Cy - self.h // 2
+
+    def move(self):
+        pass
+    
 
 class Meteorito:
     def  __init__(self):
-        self.vx = -5
+        self.vx = -0.5
         self.Cx = 800
         self.Cy = 300
         self.h = 40
         self.w = 40
-        self.posx = self.Cx - self.w //2
-        self.posy = self.Cy - self.h // 2
-
-
-        self.image  = pg.Surface((40,40))
+        self.image  = pg.Surface((self.w,self.h))
         self.image.fill(YELLOW)
 
         
 
+    @property
+    def posx(self):
+        return self.Cx - self.w //2
+    
+    @property
+    def posy(self):
+        return self.Cy - self.h // 2
+
+    def move(self, limSupX, limSupY):
+        if  self.Cx <= 0:
+            self.Cx = limSupX
+            
+        else:
+            self.Cx += self.vx
+    
 
 class Game:
     def __init__(self):
@@ -31,6 +64,7 @@ class Game:
         self.pantalla.fill(BACKGRAUND)
         pg.display.set_caption("The_quest")
         self.meteo = Meteorito()
+        self.apolo = Nave()
 
     def main_loop(self):
         game_over = False   
@@ -39,9 +73,24 @@ class Game:
             for event in pg.event.get():
                 if event.type == QUIT:
                     game_over = True
-                
 
+                if event.type == KEYDOWN:
+                    if event.key == K_UP:
+                        self.apolo.Cy -= self.apolo.vy*10
+                    if event.key == K_DOWN:
+                        self.apolo.Cy += self.apolo.vy*10
+
+            key_pressed =pg.key.get_pressed()
+            if key_pressed[K_UP]:
+                self.apolo.Cy -= self.apolo.vy
+            if key_pressed[K_DOWN]:
+                self.apolo.Cy += self.apolo.vy
+            
+            self.pantalla.fill(BACKGRAUND)   
+            
             self.pantalla.blit(self.meteo.image, (self.meteo.posx, self.meteo.posy))
+            self.pantalla.blit(self.apolo.image, (self.apolo.posx, self.apolo.posy))
+            self.meteo.move(800, 600)
             pg.display.flip()
 
 
